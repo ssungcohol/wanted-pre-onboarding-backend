3084c9d71660c3c1f38c7fd10fe0c2afe4abf1c9
sungjae <sjhj825@naver.com>
2023-08-15 21:53:00 +0900

    Feat: DB변경(Mysql AWS RDS 적용), id 생성 전략 변경

--- src/main/java/com/example/noticeboard/entity/User.java
+++ src/main/java/com/example/noticeboard/entity/User.java
@@ -16,7 +16,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class User {
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@Column(nullable = false, unique = true)
